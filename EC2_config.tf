ce10cdafa3e7d6aa9a148c1d931403ba9cd26d4b
Julen Miner <mjulen@live.com>
2020-11-11 13:32:21 +0100

    added ssh security group

--- EC2_config.tf
+++ EC2_config.tf
@@ -56,3 +56,27 @@ resource "aws_key_pair" "deployer_key" {
   key_name   = var.ec2_key_name
   public_key = var.ec2_public_key
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from outside"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
