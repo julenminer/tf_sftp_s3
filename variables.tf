061f7b79d4f96f8104904c85d877c2639d01f5ac
Julen Miner <mjulen@live.com>
2020-11-11 13:27:29 +0100

    added key pair vars

--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "region" {
 variable "bucket_name" {
   default = "bucket-for-terraform-test"
 }
+variable "ec2_key_name" {
+  default = "id_rsa"
+}
+variable "ec2_public_key" {
+  default = "ssh-rsa ..."
+}
+variable "ec2_private_key_file_path" {
+  default = "~/.ssh/id_rsa"
+}
