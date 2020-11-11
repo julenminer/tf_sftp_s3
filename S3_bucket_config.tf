3bed728333ed085f875e08137cf6d694929ec6c6
Julen Miner <mjulen@live.com>
2020-11-11 12:38:31 +0100

    added two objects to s3

--- S3_bucket_config.tf
+++ S3_bucket_config.tf
@@ -1,3 +1,12 @@
 resource "aws_s3_bucket" "test_bucket" {
   bucket = var.bucket_name
 }
+resource "aws_s3_bucket_object" "src_folder" {
+  bucket = aws_s3_bucket.test_bucket.id
+  key    = "src/"
+}
+
+resource "aws_s3_bucket_object" "images_folder" {
+  bucket = aws_s3_bucket.test_bucket.id
+  key    = "images/"
+}
